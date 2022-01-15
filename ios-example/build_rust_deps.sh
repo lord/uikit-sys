1b9780d5376335c956b84596a1f49c38df96cefd
Robert Lord <lord@users.noreply.github.com>
2022-01-15 10:53:35 -0800

    delete examples and github tests

--- ios-example/build_rust_deps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-PATH=$PATH:$HOME/.cargo/bin
-echo "$PATH"
-cd ..
-
-cargo lipo --targets x86_64-apple-ios
--- ios-example/ios-src/main.m
+++
@@ -1,6 +0,0 @@
-#import "bindings.h"
-
-int main() {
-    run_app();
-    return 0;
-}
